a7009b54584d681f214fa75bddf33a435184970b
DAWID DYRCZ <dyrcu15@gmail.com>
2015-09-21 18:21:41 +0200

    Model, View, View model + trucks view sketch

---
+++ CMS/Model/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Model
+{
+    class Car
+    {
+        public int CarID { get; set; }
+        public string Brand { get; set; }
+        public string VINNumber { get; set; }
+        public DateTime TermTechnicalResearch { get; set; }
+        public DateTime TachoLegalization { get; set; }
+        public DateTime ListUDT { get; set; }
+        public DateTime OCPolicy { get; set; }
+        public DateTime ACPolicy { get; set; }
+    }
+}
