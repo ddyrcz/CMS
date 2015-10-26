a54f3d4e57e946ae78ed8889ff33c739b93aebc2
DAWID DYRCZ <dyrcu15@gmail.com>
2015-10-26 18:43:06 +0100

    Group styles into 1 file; Startup structure; FIX date pickers background

--- CMS/View/StartupView.xaml.cs
+++ CMS/View/StartupView.xaml.cs
@@ -23,5 +23,10 @@ namespace CMS.View
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
--- CMS/View/TrucksView.xaml.cs
+++ CMS/View/TrucksView.xaml.cs
@@ -25,7 +25,38 @@ namespace CMS
     {        
         public MainWindow()
         {
-            InitializeComponent();            
+            InitializeComponent();
+
+            FixDatePickerBackground();
+        }
+
+        private void FixDatePickersBackground()
+        {
+            TechnicalResearchDatePicker.Loaded += delegate
+            {
+                var textBox = (TextBox)TechnicalResearchDatePicker.Template.FindName("PART_TextBox", TechnicalResearchDatePicker);
+                textBox.Background = TechnicalResearchDatePicker.Background;
+            };
+            TachoLegalizationDatePicker.Loaded += delegate
+            {
+                var textBox = (TextBox)TachoLegalizationDatePicker.Template.FindName("PART_TextBox", TachoLegalizationDatePicker);
+                textBox.Background = TachoLegalizationDatePicker.Background;
+            };
+            LiftUDTDatePicker.Loaded += delegate
+            {
+                var textBox = (TextBox)LiftUDTDatePicker.Template.FindName("PART_TextBox", LiftUDTDatePicker);
+                textBox.Background = LiftUDTDatePicker.Background;
+            };
+            OCDatePicker.Loaded += delegate
+            {
+                var textBox = (TextBox)OCDatePicker.Template.FindName("PART_TextBox", OCDatePicker);
+                textBox.Background = OCDatePicker.Background;
+            };
+            ACDatePicker.Loaded += delegate
+            {
+                var textBox = (TextBox)ACDatePicker.Template.FindName("PART_TextBox", ACDatePicker);
+                textBox.Background = ACDatePicker.Background;
+            };
         }
 
         private void Save_Click(object sender, RoutedEventArgs e)
